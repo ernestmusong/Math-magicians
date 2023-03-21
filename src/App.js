5a6a6835652bfab6af19ff5bb7fdd5c9bbee139b
Musong Ernest Akeh <ernestakeh@gmail.com>
2023-03-21 20:56:56 +0100

    fix linters

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
+import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import './App.css';
-import { Switch,Route} from "react-router-dom";
- 
+
 function App() {
   return (
     <>
       <Switch>
-         <Route exact path="/">
-            <Home/>
-          </Route>
-      </Switch>    
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router} from "react-router-dom";
+
 ReactDOM.render(
   <Router>
     <App />
   </Router>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 reportWebVitals();
