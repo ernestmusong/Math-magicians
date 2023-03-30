c247cadba58dd99f971e53498b092ceeeea2538e
Musong Ernest Akeh <ernestakeh@gmail.com>
2023-03-30 16:15:24 +0100

    Fix linters

---
+++ src/tests/Navbar.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Navbar from '../components/Navbar';
+
+test('Navbar renders correctly', () => {
+  const component = renderer.create(<Navbar />);
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
