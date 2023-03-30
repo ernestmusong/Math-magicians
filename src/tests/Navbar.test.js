dc9c4f7ab9e222fcc5a77480433660c9be4897ed
Musong Ernest Akeh <ernestakeh@gmail.com>
2023-03-30 20:46:36 +0100

    Revert "Fix operate.js testTest"

--- src/tests/Navbar.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Navbar from '../components/Navbar';
-
-test('Navbar renders correctly', () => {
-  const component = renderer.create(<Navbar />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-});
